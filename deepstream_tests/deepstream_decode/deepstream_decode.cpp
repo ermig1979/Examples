9b1b09491f01208c891e3df1df5eb9d7dab8cd09
Yermalayeu Ihar <ermig@tut.by>
2021-10-06 17:46:24 +0300

    +add deepstream_decode test.

--- deepstream_tests/deepstream_decode/deepstream_decode.cpp
+++ deepstream_tests/deepstream_decode/deepstream_decode.cpp
@@ -30,91 +30,11 @@
 #include "Gst/Pipeline.h"
 #include "Gst/Element.h"
 
-#define MAX_DISPLAY_LEN 64
-
-#define PGIE_CLASS_ID_VEHICLE 0
-#define PGIE_CLASS_ID_PERSON 2
-
- /* The muxer output resolution must be set if the input streams will be of
-  * different resolution. The muxer will scale all the input frames to this
-  * resolution. */
-#define MUXER_OUTPUT_WIDTH 1920
-#define MUXER_OUTPUT_HEIGHT 1080
-
-  /* Muxer batch formation timeout, for e.g. 40 millisec. Should ideally be set
-   * based on the fastest source's framerate. */
-#define MUXER_BATCH_TIMEOUT_USEC 40000
-
-gint frame_number = 0;
-gchar pgie_classes_str[4][32] = { "Vehicle", "TwoWheeler", "Person", "Roadsign" };
-
-/* osd_sink_pad_buffer_probe  will extract metadata received on OSD sink pad
- * and update params for drawing rectangle, object information etc. */
-
-static GstPadProbeReturn osd_sink_pad_buffer_probe(GstPad* pad, GstPadProbeInfo* info, gpointer u_data)
+struct Context
 {
-    GstBuffer* buf = (GstBuffer*)info->data;
-    guint num_rects = 0;
-    NvDsObjectMeta* obj_meta = NULL;
-    guint vehicle_count = 0;
-    guint person_count = 0;
-    NvDsMetaList* l_frame = NULL;
-    NvDsMetaList* l_obj = NULL;
-    NvDsDisplayMeta* display_meta = NULL;
-
-    NvDsBatchMeta* batch_meta = gst_buffer_get_nvds_batch_meta(buf);
-
-    for (l_frame = batch_meta->frame_meta_list; l_frame != NULL;
-        l_frame = l_frame->next) {
-        NvDsFrameMeta* frame_meta = (NvDsFrameMeta*)(l_frame->data);
-        int offset = 0;
-        for (l_obj = frame_meta->obj_meta_list; l_obj != NULL;
-            l_obj = l_obj->next) {
-            obj_meta = (NvDsObjectMeta*)(l_obj->data);
-            if (obj_meta->class_id == PGIE_CLASS_ID_VEHICLE) {
-                vehicle_count++;
-                num_rects++;
-            }
-            if (obj_meta->class_id == PGIE_CLASS_ID_PERSON) {
-                person_count++;
-                num_rects++;
-            }
-        }
-        display_meta = nvds_acquire_display_meta_from_pool(batch_meta);
-        NvOSD_TextParams* txt_params = &display_meta->text_params[0];
-        display_meta->num_labels = 1;
-        txt_params->display_text = (char*)g_malloc0(MAX_DISPLAY_LEN);
-        offset = snprintf(txt_params->display_text, MAX_DISPLAY_LEN, "Person = %d ", person_count);
-        offset = snprintf(txt_params->display_text + offset, MAX_DISPLAY_LEN, "Vehicle = %d ", vehicle_count);
-
-        /* Now set the offsets where the string should appear */
-        txt_params->x_offset = 10;
-        txt_params->y_offset = 12;
-
-        /* Font , font-color and font-size */
-        txt_params->font_params.font_name = "Serif";
-        txt_params->font_params.font_size = 10;
-        txt_params->font_params.font_color.red = 1.0;
-        txt_params->font_params.font_color.green = 1.0;
-        txt_params->font_params.font_color.blue = 1.0;
-        txt_params->font_params.font_color.alpha = 1.0;
-
-        /* Text background color */
-        txt_params->set_bg_clr = 1;
-        txt_params->text_bg_clr.red = 0.0;
-        txt_params->text_bg_clr.green = 0.0;
-        txt_params->text_bg_clr.blue = 0.0;
-        txt_params->text_bg_clr.alpha = 1.0;
-
-        nvds_add_display_meta_to_frame(frame_meta, display_meta);
-    }
-
-    g_print("Frame Number = %d Number of objects = %d "
-        "Vehicle Count = %d Person Count = %d\n",
-        frame_number, num_rects, vehicle_count, person_count);
-    frame_number++;
-    return GST_PAD_PROBE_OK;
-}
+    GMainLoop* loop;
+    int debugLevel;
+};
 
 static gboolean bus_call(GstBus* bus, GstMessage* msg, gpointer data)
 {
@@ -142,7 +62,7 @@ static gboolean bus_call(GstBus* bus, GstMessage* msg, gpointer data)
     default:
         break;
     }
-#if 1
+#if 0
     std::cout << "Message: ";
     std::cout << " type = " << msg->type;
     std::cout << " src = " << msg->src->name;
@@ -464,8 +384,8 @@ int main(int argc, char* argv[])
     //decoder = gst_element_factory_make("avdec_h264", "avdec_h264-decoder");
     decoder = gst_element_factory_make("nvv4l2decoder", "nvv4l2-decoder");
     
-    sink = gst_element_factory_make("nveglglessink", "video-output");
-    //sink = gst_element_factory_make("fakesink", "video-output");
+    //sink = gst_element_factory_make("nveglglessink", "video-output");
+    sink = gst_element_factory_make("fakesink", "video-output");
     
 
     if (!source || !demuxer || !parser || !decoder || !sink) {
@@ -485,7 +405,6 @@ int main(int argc, char* argv[])
 
     /* add all elements into the pipeline: file-source | ts-demuxer | h264parse | decoder | video-output */
     gst_bin_add_many(GST_BIN(pipeline), source, demuxer, parser, decoder, sink, NULL);
-
 
     /* note that the demuxer will be linked to the decoder dynamically.
      * The source pad(s) will be created at run time, by the demuxer when it detects the amount and nature of streams.
